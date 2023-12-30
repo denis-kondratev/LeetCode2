a08543bd3224cef0ecddf3b3975e18860e0bd9d2
denis-kondratev <kondratev.da@gmail.com>
2023-12-30 22:54:19 +0300

    Solve the Redistribute Characters to Make All Strings Equal problem

---
+++ 1897. Redistribute Characters to Make All Strings Equal/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * 1897. Redistribute Characters to Make All Strings Equal
+ * https://leetcode.com/problems/redistribute-characters-to-make-all-strings-equal/?envType=daily-question&envId=2023-12-30
+ *
+ * You are given an array of strings words (0-indexed). In one operation, pick two distinct indices i and j, where
+ * words[i] is a non-empty string, and move any character from words[i] to any position in words[j].
+ *
+ * Return true if you can make every string in words equal using any number of operations, and false otherwise.
+ *
+ * Example 1:
+ *
+ * Input: words = ["abc","aabc","bc"]
+ * Output: true
+ * Explanation: Move the first 'a' in words[1] to the front of words[2],
+ * to make words[1] = "abc" and words[2] = "abc".
+ * All the strings are now equal to "abc", so return true.
+ *
+ * Example 2:
+ *
+ * Input: words = ["ab","a"]
+ * Output: false
+ * Explanation: It is impossible to make all the strings equal using the operation.
+ *
+ * Constraints:
+ *
+ * 1 <= words.length <= 100
+ * 1 <= words[i].length <= 100
+ * words[i] consists of lowercase English letters.
+ */
+
+Console.WriteLine("Hello, World!");
+
+public class Solution
+{
+    public bool MakeEqual(string[] words)
+    {
+        var counters = new int[26];
+        
+        foreach (var word in words)
+        {
+            foreach (var c in word)
+            {
+                counters[c - 'a']++;
+            }
+        }
+
+        var wordCount = words.Length;
+        
+        foreach (var counter in counters)
+        {
+            if (counter % wordCount != 0)
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
