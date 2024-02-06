b3ab63419abe98ae194444bc7c4030acc98503bf
denis-kondratev <kondratev.da@gmail.com>
2024-02-06 21:56:56 +0300

    Solve the Group Anagrams problem

---
+++ 49. Group Anagrams/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * 49. Group Anagrams
+ * https://leetcode.com/problems/group-anagrams/?envType=daily-question&envId=2024-02-06
+ *
+ * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+ * typically using all the original letters exactly once.
+ *
+ * Example 1:
+ *   Input: strs = ["eat","tea","tan","ate","nat","bat"]
+ *   Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ *
+ * Example 2:
+ *   Input: strs = [""]
+ *   Output: [[""]]
+ *
+ * Example 3:
+ *   Input: strs = ["a"]
+ *   Output: [["a"]]
+ *
+ * Constraints:
+ *   1 <= strs.length <= 10^4
+ *   0 <= strs[i].length <= 100
+ *   strs[i] consists of lowercase English letters.
+ */
+
+Console.WriteLine("Hello, World!");
+
+public class Solution
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        var anagrams = new Dictionary<string, IList<string>>();
+
+        foreach (var str in strs)
+        {
+            var key = string.Concat(str.OrderBy(c => c));
+
+            if (anagrams.TryGetValue(key, out var list))
+            {
+                list.Add(str);
+            }
+            else
+            {
+                anagrams[key] = new List<string> { str };
+            }
+        }
+
+        return anagrams.Select(p => p.Value).ToArray();
+    }
+}
