e1a82c50bd1d25bba78d837fe76606d68022335c
Denis Kondratev <kondratev.da@gmail.com>
2025-03-09 18:58:30 +0100

    Add the 3208. Alternating Groups II problem

---
+++ 3208. Alternating Groups II/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * 3208. Alternating Groups II
+ * https://leetcode.com/problems/alternating-groups-ii/description/?envType=daily-question&envId=2025-03-09
+ *
+ * There is a circle of red and blue tiles. You are given an array of integers colors and an integer k.
+ * The color of tile i is represented by colors[i]:
+ *   colors[i] == 0 means that tile i is red.
+ *   colors[i] == 1 means that tile i is blue.
+ *
+ * An alternating group is every k contiguous tiles in the circle with alternating colors (each tile in the
+ * group except the first and last one has a different color from its left and right tiles).
+ *
+ * Return the number of alternating groups.
+ * Note that since colors represents a circle, the first and the last tiles are considered to be next to each other.
+ *
+ * Constraints:
+ *   3 <= colors.length <= 105
+ *   0 <= colors[i] <= 1
+ *   3 <= k <= colors.length
+ */
+
+Console.WriteLine("Hello, World!");
+
+public class Solution
+{
+    public int NumberOfAlternatingGroups(int[] colors, int k)
+    {
+        return 0;
+    }
+}
