eecae0644b80409957d0c52264aaeda48f15c244
Denis Kondratev <kondratev.da@gmail.com>
2025-04-14 21:58:22 +0200

    [1534] Implement solution for the problem: Count Good Triplets

--- 1534. Count Good Triplets/Program.cs
+++ 1534. Count Good Triplets/Program.cs
@@ -36,6 +36,24 @@ public class Solution
 {
     public int CountGoodTriplets(int[] arr, int a, int b, int c)
     {
-        return 0;
+        var count = 0;
+        
+        for (var i = 0; i < arr.Length; i++)
+        {
+            for (var j = i + 1; j < arr.Length; j++)
+            {
+                for (var k = j + 1; k < arr.Length; k++)
+                {
+                    if (Math.Abs(arr[i] - arr[j]) <= a 
+                        && Math.Abs(arr[j] - arr[k]) <= b 
+                        && Math.Abs(arr[i] - arr[k]) <= c)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+        
+        return count;
     }
 }
