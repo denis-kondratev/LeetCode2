ca2b8220703a4271ac2f1057e54c864abdcb98b7
Denis Kondratev <kondratev.da@gmail.com>
2025-04-19 23:30:49 +0200

    [2563] Implement solution for the problem: Count the Number of Fair Pairs

--- 2563. Count the Number of Fair Pairs/Program.cs
+++ 2563. Count the Number of Fair Pairs/Program.cs
@@ -25,12 +25,35 @@
  *   -10^9 <= lower <= upper <= 10^9
  */
 
-Console.WriteLine("Hello, World!");
+var solution = new Solution();
+Console.WriteLine(solution.CountFairPairs([0, 1, 4, 4, 5, 7], 3, 6));
 
 public class Solution
 {
     public long CountFairPairs(int[] nums, int lower, int upper)
     {
-        return 0;
+        Array.Sort(nums);
+        return CountPairs(nums, upper) - CountPairs(nums, lower - 1);
+    }
+
+    private long CountPairs(int[] nums, int target)
+    {
+        long count = 0;
+        int left = 0, right = nums.Length - 1;
+
+        while (left < right)
+        {
+            if (nums[left] + nums[right] > target)
+            {
+                right--;
+            }
+            else
+            {
+                count += right - left;
+                left++;
+            }
+        }
+
+        return count;
     }
 }
